614533d5142392f8e9a501febedc63fc9d37ddea
michaelvitucci <michaelvitucci1@gmail.com>
2019-08-28 19:19:37 -0500

    Search with title and year

--- plex_collections.py
+++ plex_collections.py
@@ -2,14 +2,15 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from plexapi.server import PlexServer
 from plexapi.exceptions import NotFound
+import math
 
 # the name of your movie library and spreadshee
 LIBRARY = <YOUR MOVIES LIBRARY> # Ex: 'Movies'
-GSPREAD_NAME = <YOUR SPREADSHEET NAME> # Ex: 'Movie Collections'
+GSPREAD_NAME = <SPREADSHEET NAME> # Ex: 'Movie Collections'
 
 # use creds to create a client to interact with your local Plex server
 PLEXURL = <PLEX URL> # Ex: 'http://xxx.xxx.x.x:32400'
-TOKEN = <YOUR PLEX TOKEN> # Plex Token
+TOKEN = <PLEX TOKEN> # Plex Token
 
 
 # create an instance of PlexServer
@@ -24,6 +25,11 @@ client = gspread.authorize(creds)
 sheet = client.open(GSPREAD_NAME).sheet1
 sheet = sheet.get_all_records()
 
+def record_missing():
+  f= open("missing.txt","a+")
+  f.write(row['Movies'] + '\n')
+  f.close
+
 # open and clear a file to record missing movies
 f= open("missing.txt","w").close()
 
@@ -31,12 +37,12 @@ f= open("missing.txt","w").close()
 for row in sheet:
   if len(str(row['Collection Name'])) > 1:
     collection_name = str(row['Collection Name'])
-
   if len(str(row['Movies'])):
-    try:
-      plex.library.section(LIBRARY).get(str(row['Movies'])).addCollection(collection_name)
-      print("Adding {} to {}".format(row['Movies'], collection_name))
-    except NotFound:
-      f= open("missing.txt","a+")
-      f.write(row['Movies'] + '\n')
-      f.close
+    if isinstance(row['Year'], str):
+      record_missing()
+    else:
+      try:
+        plex.library.section(LIBRARY).search(str(row['Movies']), year=row['Year'])[0].addCollection(collection_name)
+        print("Adding {} to {}".format(row['Movies'], collection_name))
+      except (NotFound, IndexError):
+        record_missing()
